8c2373c20ca15c398ab1a3475c3afcfcfb25ef1c
levywanke <levywanke@gmail.com>
2024-03-22 15:41:53 +0300

    fixed files

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -18,5 +18,6 @@ def hbnb():
     """Render HBNB"""
     return 'HBNB'
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """Renders a template displaying states and cities sorted alphabetically."""
+    """Renders a template displaying states and
+        cities sorted alphabetically."""
     states = storage.all("State").values()
     return render_template('8-cities_by_states.html', states=states)
 
@@ -19,6 +20,7 @@ def cities_by_states():
 def teardown_db(exception):
     """Closes the storage connection upon teardown."""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
